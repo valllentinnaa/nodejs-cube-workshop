48c6441642647bab7bf1f7b1f1555fbe5166c2de
valllentinnaa <valllentinnaa@gmail.com>
2020-06-07 21:37:58 +0300

    feat: create cube model and save cube in the db

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development'
 
-const config = require('./config/config')[env];
-const app = require('express')();
+const config = require('./config/config')[env]
+const express = require('express')
+const app = express()
 
-require('./config/express')(app);
-require('./config/routes')(app);
+require('./config/express')(app)
+require('./config/routes')(app)
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`))
